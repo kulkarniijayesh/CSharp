42d2a56bfbe018ff67f9319b2b17443e85057289
kulkarniijayesh <kulkarniijayesh@gmail.com>
2017-03-11 11:12:32 +0530

    Adding a class library to hold all the classes

---
+++ ClassLibraryCSharp/ClassLibraryCSharp/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryCSharp
+{
+    public class Class1
+    {
+    }
+}
